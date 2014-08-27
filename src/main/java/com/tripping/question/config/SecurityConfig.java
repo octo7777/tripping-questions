b7db5c991aac8b88909b960175987814cca396f5
Tomasz Szuba <tomasz.szuba@coconet.pl>
2014-08-27 21:34:12 +0200

    use form login

--- src/main/java/com/tripping/question/config/SecurityConfig.java
+++ src/main/java/com/tripping/question/config/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.tripping.question.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 import com.tripping.question.core.domain.User;
 import com.tripping.question.core.repository.UserRepository;
@@ -18,30 +20,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserRepository userRepository;
-    
+
     @Autowired
     private UserDetailsServiceImplementation userDetailsServiceImplementation;
 
     @Override
     protected void registerAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-      if (userRepository.count() == 0) {
-          userRepository.save(new User("Bolek", "bolek@tripper.com", "pass", Roles.ADMIN));
-      }
-      DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-      daoAuthenticationProvider.setUserDetailsService(userDetailsServiceImplementation);
+        if (userRepository.count() == 0) {
+            userRepository.save(new User("Bolek", "bolek@tripper.com", "pass", Roles.ADMIN));
+        }
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsServiceImplementation);
+        //daoAuthenticationProvider.setPasswordEncoder(new StandardPasswordEncoder());
 
-      auth.authenticationProvider(daoAuthenticationProvider);
+        auth.authenticationProvider(daoAuthenticationProvider);
 
 //    auth.inMemoryAuthentication()
 //        .withUser("Bolek").password("pass").roles("USER").and()
 //            .withUser("Lolek").password("pass").roles("USER");
-  }
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeUrls()
+            .antMatchers("protected.html").hasRole(Roles.ADMIN)
             .anyRequest().anonymous()
             .and()
-            .httpBasic();
+            .formLogin();
     }
 }
