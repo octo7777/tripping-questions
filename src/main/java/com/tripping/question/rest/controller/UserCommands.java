24da1c3f056ff2f52c2cc7a060153181a7aa6a5c
Tomasz Szuba <tomasz.szuba@coconet.pl>
2014-08-27 21:21:07 +0200

    make it possible to add user

--- src/main/java/com/tripping/question/rest/controller/UserCommands.java
+++ src/main/java/com/tripping/question/rest/controller/UserCommands.java
@@ -1,8 +1,26 @@
 package com.tripping.question.rest.controller;
 
+import java.math.BigInteger;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller()
+import com.tripping.question.core.domain.User;
+import com.tripping.question.core.repository.UserRepository;
+
+@Controller
+@RequestMapping("/users")
 public class UserCommands {
+    @Autowired
+    private UserRepository userRepository;
 
+    @RequestMapping(method = RequestMethod.PUT)
+    @ResponseBody
+    public BigInteger addUser(@RequestBody User user) {
+        userRepository.save(user);
+    }
 }
