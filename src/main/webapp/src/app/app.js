b8314f17ef9d7d77c67a2505a57a473f7e5608a2
Krzysztof Sakowski <Krzysztof.Sakowski@coconet.pl>
2014-08-28 00:24:44 +0200

    finally landing page is bearable

--- src/main/webapp/src/app/app.js
+++ src/main/webapp/src/app/app.js
@@ -2,8 +2,31 @@ var gamesApp = angular.module('gamesApp', [
    'ngRoute',
    'gamesAppControllers',
    'game1Controller',
-   'usersService'
+   'usersService',
+   'ui.bootstrap'
 ]);
+
+function DropdownCtrl($scope) {
+  $scope.items = [
+    'The first choice!',
+    'And another choice for you.',
+    'but wait! A third!'
+  ];
+
+  $scope.status = {
+    isopen: false
+  };
+
+  $scope.toggled = function(open) {
+    console.log('Dropdown is now: ', open);
+  };
+
+  $scope.toggleDropdown = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.status.isopen = !$scope.status.isopen;
+  };
+}
 
 gamesApp.config(['$routeProvider',
            function($routeProvider) {
